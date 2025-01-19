6edbc8c786b9d2af3530ec09e9635108af4a98a9
SM-Codaholic98 <162802446+SM-Codaholic98@users.noreply.github.com>
2025-01-19 15:23:05 +0530

    Vco2EP

---
+++ Vco2EP_app.bat
@@ -0,0 +1,3 @@
+cd D:\EDA\Vehicle_CO2_Emission_Predictor v1.0
+python Vco2EP_app.py
+pause
---
+++ test.py
@@ -0,0 +1,4 @@
+s = 'AS50'
+print()
+print(filter(str.isdigit, s))
+print(''.join(filter(str.isdigit, s)))
